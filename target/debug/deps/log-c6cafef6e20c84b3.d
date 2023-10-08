9c8e324bdada8ce42a40cc57343c7b6abf5958b1
Gavin McDiarmid <gmcdiarm@calpoly.edu>
2023-10-07 17:36:55 -0700

    10/7

--- target/debug/deps/log-c6cafef6e20c84b3.d
+++
@@ -1,8 +0,0 @@
-/home/gavin/Dev/chop/target/debug/deps/log-c6cafef6e20c84b3.rmeta: /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/lib.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/macros.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/serde.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/__private_api.rs
-
-/home/gavin/Dev/chop/target/debug/deps/log-c6cafef6e20c84b3.d: /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/lib.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/macros.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/serde.rs /home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/__private_api.rs
-
-/home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/lib.rs:
-/home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/macros.rs:
-/home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/serde.rs:
-/home/gavin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.20/src/__private_api.rs:
